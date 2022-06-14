e6aad21e21eadcf1dcb53f2fdac16ba3c66e4e3e
Andrew Chang <acchang4@gmail.com>
2022-06-14 19:55:02 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+
+const dotenv = require('dotenv') // .env file
+dotenv.config() // using .env
+const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PW}@cluster0.sgg7p.mongodb.net/?retryWrites=true&w=majority`;
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('my-trees')
+    const treesCollection = db.collection('trees')
+    
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.post('/trees', (req, res) => {
+        treesCollection.insertOne(req.body)
+          .then(result => {
+            console.log(result)
+          })
+          .catch(error => console.error(error))
+      })
+
+    app.get('/', (req, res) => {
+        res.sendFile('/Users/andrewchang/the_odin_project/OrchardTracker' + '/index.html')
+        })
+
+    app.listen(3000, function() {
+        console.log('listening on 3000')
+        })
+
+  })
+  .catch(error => console.error(error))
