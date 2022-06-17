b8d1b0a6cd18c4ed12141b5c0506cfd451f1e80b
Andrew Chang <acchang4@gmail.com>
2022-06-16 22:36:12 -0400

    key/value?

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // require('dotenv').config();
 
-const connectionString = `mongodb+srv://${USERNAME}:${PW}@cluster0.sgg7p.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${KEY}:${VALUE}@cluster0.sgg7p.mongodb.net/?retryWrites=true&w=majority`;
 
 // const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PW}@cluster0.sgg7p.mongodb.net/?retryWrites=true&w=majority`;
 // const connectionString = `mongodb+srv://heroku:heroku@cluster0.sgg7p.mongodb.net/?retryWrites=true&w=majority`;
