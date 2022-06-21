213ef2c25c3367e5b3873b9a03880e20b3b41514
Andrew Chang <acchang4@gmail.com>
2022-06-21 13:22:59 -0400

    fixed, needed bodyparser

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 if(process.env.NODE_ENV !== 'production'){
   const dotenv = require('dotenv')
   dotenv.config({ debug: true })
-  // dotenv.config({path: 'vars.env'})
+// if process.env.NODE_ENV == production
+// then we are in Heroku and dotenv is not required
+// we use the config var for DATABASE_URL as the MongoClient password.
+// In Node.js, use process.env to access environment variables
 }
 
 const express = require('express');
@@ -9,12 +12,8 @@ const app = express();
 const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient
 
-// const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.sgg7p.mongodb.net/?retryWrites=true&w=majority`;
-// const connectionString = `mongodb+srv://heroku:heroku@cluster0.sgg7p.mongodb.net/?retryWrites=true&w=majority`;
-
 MongoClient.connect(process.env.DATABASE_URL, { useUnifiedTopology: true})
 
-// MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('my-trees')
@@ -25,13 +24,6 @@ MongoClient.connect(process.env.DATABASE_URL, { useUnifiedTopology: true})
     app.use(express.static('public'))
     app.use(bodyParser.json())
 
-    // app.use(express.urlencoded({ extended: true }))
-    // app.use(express.json())
-
-
-    // app.listen(3000, function() {
-    //   console.log('listening on 3000')
-    //   })
 
     let port = process.env.PORT;
     if (port == null || port == "") {
@@ -138,143 +130,3 @@ MongoClient.connect(process.env.DATABASE_URL, { useUnifiedTopology: true})
   })
   .catch(error => console.error(error))
 
-
-  /* newer code:
-
-  if(process.env.NODE_ENV !== 'production'){
-  const dotenv = require('dotenv')
-  dotenv.config({ debug: true })
-// if process.env.NODE_ENV == production
-// then we are in Heroku and dotenv is not required
-// we use the config var for DATABASE_URL as the MongoClient password.
-// In Node.js, use process.env to access environment variables
-}
-
-const express = require('express');
-const app = express();
-const MongoClient = require('mongodb').MongoClient
-
-MongoClient.connect(process.env.DATABASE_URL, { useUnifiedTopology: true})
-
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('my-trees')
-    const treesCollection = db.collection('trees')
-    
-    app.set('view engine', 'ejs')
-    // app.use(bodyParser.urlencoded({ extended: true }))
-    // app.use(express.static('public'))
-    // app.use(bodyParser.json())
-
-    app.use(express.urlencoded({ extended: true }))
-    app.use(express.json())
-
-
-    // app.listen(3000, function() {
-    //   console.log('listening on 3000')
-    //   })
-
-    let port = process.env.PORT;
-    if (port == null || port == "") {
-      port = 8000;
-    }
-    app.listen(port);
-
-
-      app.post('/tree-record', (req, res) => {
-        treesCollection.insertOne(
-          {date: req.body.date, plot: req.body.plot, 
-            variety: req.body.variety, yield: 0
-          })
-          .then(result => {
-            console.log('plot added')
-            res.redirect('/')
-          })
-          .catch(error => console.error(error))
-      })
-
-
-      app.get('/', (req, res) => {
-        db.collection('trees').find().toArray()
-        .then(results => {
-          res.render('index.ejs', { trees: results })
-        })
-        .catch(error => console.error(error))
-      }) 
-
-
-      app.put('/trees', (req, res) => {
-        treesCollection.findOneAndUpdate(
-          { variety: 'corn' },
-          {
-            $set: {
-              variety: req.body.variety,
-              date: req.body.date
-            }
-          },
-          {
-            upsert: false
-            // Upsert false so it doesn't just place a new one if none exists
-          }
-        )
-        .then(result => {
-          if (res.ok) return res.json()
-        })
-        .then(result => {
-            // console.log(res)
-            res.json('Success')
-           })
-          .catch(error => console.error(error))
-      })
-
-
-      app.delete('/trees', (req, res) => {
-        treesCollection.deleteOne(
-          { variety: req.body.variety },
-          )
-          .then(result => {
-            if (result.deletedCount === 0) {
-              return res.json('No cukes to delete')
-            }
-            res.json(`Deleted cukes`)
-          })
-          .catch(error => console.error(error))
-      })
-
-      app.delete('/deletePlot', (request, response) => {
-        db.collection('trees').deleteOne({
-          date: request.body.DateDelete,
-          plot: request.body.PlotDelete,
-          variety: request.body.VarietyDelete
-        })
-        .then(result => {
-            console.log('Plot Deleted')
-            response.json('Plot Deleted')
-        })
-        .catch(error => console.error(error))
-      })
-
-
-      app.put('/addOneYield', (request, response) => {
-        db.collection('trees').updateOne({
-          date: request.body.DateUpdate, 
-          plot: request.body.PlotUpdate,
-          variety: request.body.VarietyUpdate,
-          yield: request.body.YieldUpdate},{
-            $set: {
-                yield:request.body.YieldUpdate + 1
-              }
-        },{
-            sort: {_id: -1},
-            upsert: true
-        })
-        .then(result => {
-            console.log('Added One')
-            response.json('Yield Added')
-        })
-        .catch(error => console.error(error))
-      })
-
-
-  })
-  .catch(error => console.error(error))
